016935c7597a4e906fef2f26c620e0349f6f1d9b
James Regis <james.regis@kyndryl.com>
2026-03-21 17:00:20 -0400

    Update environment variable names for admin password consistency

--- create-default-context-and-user.sh
+++ create-default-context-and-user.sh
@@ -14,16 +14,16 @@ UBlue='\033[4;34m'
 CONTEXT_NAME="Example Context"
 DOMAIN="example.com"
 
-OX_ADMIN_MASTER_PASSWORD=`yq e '.spec.containers[] | select (.name=="appsuite").env[] | select (.name=="OX_ADMIN_PASSWORD") | .value' Pod.yml`
-OX_ADMIN_PASSWORD=`yq e '.spec.containers[] | select (.name=="appsuite").env[] | select (.name=="OX_ADMIN_PASSWORD") | .value' Pod.yml`
+OX_ADMIN_MASTER_PASSWORD=`yq e '.spec.containers[] | select (.name=="appsuite").env[] | select (.name=="OX_CONTEXT_ADMIN_PASSWORD") | .value' Pod.yml`
+OX_CONTEXT_ADMIN_PASSWORD=`yq e '.spec.containers[] | select (.name=="appsuite").env[] | select (.name=="OX_CONTEXT_ADMIN_PASSWORD") | .value' Pod.yml`
 
 echo -e "${BRed} Create a new context ... ${NC}\n"
 
-podman exec open-xchange-appsuite /opt/open-xchange/sbin/createcontext -A oxadminmaster -P ${OX_ADMIN_MASTER_PASSWORD} -c 2 -u oxadmin -d ${CONTEXT_NAME} -g Admin -s User -p ${OX_ADMIN_PASSWORD} -L ${DOMAIN} -e oxadmin@${DOMAIN} -q 1024 --contextname ${DOMAIN} --access-combination-name=all
+podman exec open-xchange-appsuite /opt/open-xchange/sbin/createcontext -A oxadminmaster -P ${OX_ADMIN_MASTER_PASSWORD} -c 2 -u oxadmin -d ${CONTEXT_NAME} -g Admin -s User -p ${OX_CONTEXT_ADMIN_PASSWORD} -L ${DOMAIN} -e oxadmin@${DOMAIN} -q 1024 --contextname ${DOMAIN} --access-combination-name=all
 
 
 echo -e "${BRed} Create a new user ... ${NC}\n"
-podman exec open-xchange-appsuite /opt/open-xchange/sbin/createuser  --username 'john' --displayname 'John(example simple)' --givenname 'John' --surname 'Doe' --password tototiti001 --email 'john@example.com' --imaplogin 'john@example.com' --contextid 2 -A oxadmin -P ${OX_ADMIN_PASSWORD}
+podman exec open-xchange-appsuite /opt/open-xchange/sbin/createuser  --username 'john' --displayname 'John(example simple)' --givenname 'John' --surname 'Doe' --password tototiti001 --email 'john@example.com' --imaplogin 'john@example.com' --contextid 2 -A oxadmin -P ${OX_CONTEXT_ADMIN_PASSWORD}
 
 echo -e  "You can now login with the following credentials: \n"
 echo -e "login: ${BGreen}john@example.com${NC}\n"
